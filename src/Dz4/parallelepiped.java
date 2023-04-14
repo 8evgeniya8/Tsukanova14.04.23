19289a71beb114f39efe829eb9113f15e2a1683c
Tsukanova Evgeniya <tata@gmail.com>
2023-04-14 09:27:12 +0300

    dz parallelepiped

---
+++ src/Dz4/parallelepiped.java
@@ -0,0 +1,12 @@
+package Dz4;
+
+public class parallelepiped {
+    public static void main(String[]args){
+        int zminna1 = 4;
+        int zminna2 = 8;
+        int zminna3 = 12;
+        int length=zminna1*zminna2*zminna3;
+        int volume=length;
+    System.out.println("Об'єм паралелепіпеда = " + volume);
+    }
+}
